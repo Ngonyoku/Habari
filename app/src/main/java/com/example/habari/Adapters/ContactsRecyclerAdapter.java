dcb05687227f5338cad3cc57653a8bea2cbe38a0
Ngonyoku <47557555+Ngonyoku@users.noreply.github.com>
2020-05-11 03:06:56 +0400

    dial Layout Completed.

--- app/src/main/java/com/example/habari/Adapters/ContactsRecyclerAdapter.java
+++ app/src/main/java/com/example/habari/Adapters/ContactsRecyclerAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,9 +27,15 @@ public class ContactsRecyclerAdapter extends RecyclerView.Adapter<ContactsRecycl
     @NonNull
     @Override
 
-    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ContactsViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
         View v = LayoutInflater.from(mContext).inflate(R.layout.item_contacts, parent, false);
-        ContactsViewHolder viewHolder = new ContactsViewHolder(v);
+        final ContactsViewHolder viewHolder = new ContactsViewHolder(v);
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         return viewHolder;
     }
 
